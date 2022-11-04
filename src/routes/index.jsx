a6e5890bcbce01389469cb6aa39a5cd4bc8fb103
Lucas Lara <lucast_lara@hotmail.com>
2022-11-04 18:01:26 -0300

    dashboard route added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      {/* <Route element={<ProtectedRoutes />}> */}
+      <Route path="/home" element={<Dashboard />} />
+      {/* </Route> */}
     </Routes>
   );
 };
