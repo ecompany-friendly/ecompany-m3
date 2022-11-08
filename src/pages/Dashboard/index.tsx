57306ec674f80c16736401552940289e973411e7
Mateus Rezende <mateusrezende1997@hotmail.com>
2022-11-08 00:56:50 -0300

    feat: modal de info do usuario atualizado e adicionada função de logout na dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,70 +1,67 @@
-import { StyledDashboard, Profile, Elipse, Background } from "./styles";
-import darkicon from "../../assets/Logo.svg";
-
+// import MaterialList from "../../components/MaterialList";
+import { StyledDashboard, Profile, Elipse } from "./styles";
+// import darkicon from "../../assets/ecology-maps-and-location-svgrepo-com 1.svg";
 import profile from "../../assets/blank-profile-picture-973460.svg";
 import logout from "../../assets/Exit_1_.svg";
 import addmaterial from "../../assets/Group.svg";
 import lupapesquisa from "../../assets/Group(1).svg";
 import elipse from "../../assets/Ellipse 1.svg";
-import background from "../../assets/Rectangle 39.svg";
-
+// import Background from "../../assets/Rectangle 39.svg";
 import { StyledUserDataModal } from "../../components/UserDataModal/style";
 import { UserDataModal } from "../../components/UserDataModal/UserDataModal";
-
-import MaterialList from "../../components/ProductList";
-import ProductList from "../../components/ProductList";
-import { NewProduct } from "../../components/NewProduct";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const teste = () => {
-    alert("sfjsdfb");
+  const navigate = useNavigate();
+
+  const getOut = () => {
+    window.localStorage.clear();
+    navigate("/");
   };
 
   return (
     <>
       <StyledDashboard>
         <Elipse src={elipse} alt="imagem da elipse verde do fundo" />
-        <nav className="dash-nav">
-          <div className="logo">
-            <img
-              src={darkicon}
-              alt="imagem da logo em modo escuro da ecompany friendly"
-            />
-            <h1>eCOMPANY friendly</h1>
-          </div>
-          <div className="interative">
-            <div className="user-info">
-              <div className="user">
-                <Profile
-                  src={profile}
-                  alt="imagem do perfil do usuário logado"
-                ></Profile>
-                <h2>Nicolly Alves</h2>
-              </div>
-              <img src={logout} alt="imagem para fazer logout na conta" />
-            </div>
-            <div className="search">
-              <input type="text" />
+        {/* <Background src={background} alt="fundo preto" /> */}
+        <div className="container">
+          <nav className="dash-nav">
+            <div className="logo">
               <img
-                className="lupa"
-                src={lupapesquisa}
-                alt="imagem da lupa de pesquisa para filtrar material"
+                // src={darkicon}
+                alt="imagem da logo em modo escuro da ecompany friendly"
               />
-              <button type="button" className="newProduct" onClick={teste}>
+              <h1>eCOMPANY friendly</h1>
+            </div>
+            <div className="interative">
+              <div className="user-info">
+                <div className="user">
+                  <Profile
+                    src={profile}
+                    alt="imagem do perfil do usuário logado"
+                  ></Profile>
+                  <h2>Nicolly Alves</h2>
+                </div>
+                <button className="logout" onClick={getOut}>
+                  <img src={logout} alt="imagem para fazer logout na conta" />
+                </button>
+              </div>
+              <div className="search">
+                <input type="text" />
+                <img
+                  className="lupa"
+                  src={lupapesquisa}
+                  alt="imagem da lupa de pesquisa para filtrar material"
+                />
                 <img
                   src={addmaterial}
                   alt="imagem para publicar novo material"
                 />
-
               </div>
-              </button>
             </div>
           </nav>
-          <div className="modals">
-            <UserDataModal />
-          </div>
-        <NewProduct />
-        <ProductList />
+          <main>ul</main>
+        </div>
       </StyledDashboard>
     </>
   );
